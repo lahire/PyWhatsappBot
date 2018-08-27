8e1c3cdd0c015970a4d08220d8ea13f9d3e571da
Glitch (wealthy-plate) <none>
2018-08-27 14:07:50 +0000

    🍗👨‍👨‍👦 Checkpoint ./package.json:65847/1330 ./server.js:65847/817

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-
+var request = require('request');
 
 
 // Download the helper library from https://www.twilio.com/docs/node/install
@@ -16,21 +16,26 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 app.use(express.static('public'));
 
-client.messages
-      .create({
-        body: 'Hello there!',
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+919725633422'
-      })
-      .then(message => console.log(message.sid))
-      .done();
+// client.messages
+//       .create({
+//         body: 'Hello there!',
+//         from: 'whatsapp:+14155238886',
+//         to: 'whatsapp:+919725633422'
+//       })
+//       .then(message => console.log(message.sid))
+//       .done();
 
 
 app.post('/incoming', (req, res) => {
   const twiml = new MessagingResponse();
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
+  request('https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q='+req.body.Body, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    twiml.message(`
+*`+body+`*
+`);
+  });
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
