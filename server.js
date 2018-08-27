969c033d896ff6d8bb3fa0d2118c7824875e4a6f
Glitch (wealthy-plate) <none>
2018-08-27 13:57:39 +0000

    🍩🙉 Checkpoint ./server.js:65847/773

--- server.js
+++ server.js
@@ -10,30 +10,37 @@ var app = express();
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 const accountSid = 'ACde60019f8d64eac48ea794f384858795';
-const authToken = 'your_auth_token';
+const authToken = '190461bdcf6989539c97085afee01524';
 const client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.use(express.static('public'));
 
 client.messages
       .create({
         body: 'Hello there!',
         from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+15005550006'
+        to: 'whatsapp:+919725633422'
       })
       .then(message => console.log(message.sid))
       .done();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.post('/incoming', (req, res) => {
+  const twiml = new MessagingResponse();
 
-// http://expressjs.com/en/starter/basic-routing.html
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
+
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
