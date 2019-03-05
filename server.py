2b176125018cd12516d38b35ac028d3d3832dea8
Glitch (mud-gemini) <none>
2019-03-05 23:29:57 +0000

    👳🎭 Checkpoint ./requirements.txt:1307225/15 ./server.py:1307225/204

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hola Mundo!!"
 
-@app.route("/incoming")
+@app.route("/incoming", methods=["POST"])
 def incomming():
+  print('Hola')
+  print(dir(request))
+  print(request.url)
   return 'Hola Wassap'
 
 
