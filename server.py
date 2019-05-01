64c6fc5a400913a026efdeec4e14823def683268
Nicolás Giorgetti <niko.kane@gmail.com>
2019-05-01 16:57:44 -0300

    add phonenumbers parsing. TODO: redirect to specific wikipedia

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 # WikiBOT
 VERSION="0.1.1toolforge"
 
@@ -8,13 +9,24 @@ import os
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 import yaml
+import sys
+import phonenumbers
+from phonenumbers.phonenumberutil import (
+    region_code_for_country_code,
+    region_code_for_number,
+)
+
+try:
+  config = sys.argv[1] #Captures config file for local use
+except IndexError:
+  config='config.yaml'
 
 app = Flask(__name__)
 #Changes for toolfoger
 __dir__ = os.path.dirname(__file__)
 
 app.config.update(
-    yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+    yaml.safe_load(open(os.path.join(__dir__, config))))
 
 app.SID = app.config['SID']
 app.KEY = app.config['KEY']
@@ -56,7 +68,10 @@ def wikipedia_lookup(lookup, lang='es'):
     print('Connection lost with Wikipedia API!: Return code: {0}'.format(r.status_code))
     return 'Connection lost with Wikipedia API!: Return code: {0}'.format(r.status_code)
   resultado = r.json() #paso a json el POST
-  print(resultado) #console.log
+  try:
+    print(resultado) #console.log
+  except UnicodeEncodeError:
+    print('No se pudo imprimir resultado')
   try:
     pageid=list(resultado['query']['pages'].values())[0]['pageid'] #consigo el pageid
   except KeyError: #Si no se encuentra la busqueda, pageid no existe en la colección que devuelve la API
@@ -77,13 +92,19 @@ def wikipedia_lookup(lookup, lang='es'):
   'es':'*{0}*\n{1}\nMás info acá: {2}'.format(title,extract,url), 
   'en':'*{0}*\n{1}\nMore info here: {2}'.format(title, extract,url)  
   } #el mensaje que se construye. Se podrian agregar más idiomas o modificar desde acá
-  print(MENSAJE[lang])
+  print(MENSAJE[lang].encode('utf-8'))
   return MENSAJE[lang] #devuelvo el mensaje para escribir de vuelta
-  
-  
-  
-  
-  
+
+def parse_body(body, part="Body"):
+  """
+  Parses the body of the request to capture relevant data
+  body: the request form
+  part: request.form[part], i.e. body, from, api_version. Default: Body
+  returns the value of said part 
+  """
+  component = body[part]
+  return component
+
 
 @app.route("/")
 def hello():
@@ -91,7 +112,42 @@ def hello():
 
 @app.route("/incoming", methods=["POST"])
 def incoming():
-  cuerpo = request.form['Body']
+  """
+Captures the body of the message
+
+Example request.form:
+{
+  "account_sid": "ACf4c269b45de4664b2fdcc5386cc5d54b",
+  "api_version": "2010-04-01",
+  "body": "Hello! 👍",
+  "date_created": "Thu, 30 Jul 2015 20:12:31 +0000",
+  "date_sent": "Thu, 30 Jul 2015 20:12:33 +0000",
+  "date_updated": "Thu, 30 Jul 2015 20:12:33 +0000",
+  "direction": "outbound-api",
+  "error_code": null,
+  "error_message": null,
+  "from": "+14155552345",
+  "messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "num_media": "0",
+  "num_segments": "1",
+  "price": -0.00750,
+  "price_unit": "USD",
+  "sid": "MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "status": "sent",
+  "subresource_uris": {
+    "media": "/2010-04-01/Accounts/ACf4c269b45de4664b2fdcc5386cc5d54b/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json"
+  },
+  "to": "+14155552345",
+  "uri": "/2010-04-01/Accounts/ACf4c269b45de4664b2fdcc5386cc5d54b/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json"
+}
+"""
+  form = request.form 
+  cuerpo = parse_body(form, 'Body')
+  phone_number = phonenumbers.parse(parse_body(form, 'From'))
+  region_code = phone_number.country_code
+  country_code = region_code_for_country_code(region_code)  
+ # print('Telefono: {0}'.format(phone_number))
+ # print('Pais: {0}  Region: {1}'.format(country_code, region_code ))
   resp = MessagingResponse()
   if 'ayuda' in cuerpo.lower():
     resp.message(welcome_user('es'))
